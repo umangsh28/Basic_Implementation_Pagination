c19b592bd342e9cdbac43d546d9e53e9e6497278
umangsh28 <im.mrstark08@gmail.com>
2021-10-18 14:57:52 +0530

    comit2 @2:57pm

--- app/src/main/java/com/example/codingoct18/MainActivity.kt
+++ app/src/main/java/com/example/codingoct18/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.codingoct18
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.codingoct18.databinding.ActivityMainBinding
+import com.example.codingoct18.viewmodel.ViewModell
+import com.example.codingoct18.viewmodel.viewModelfact
 
 class MainActivity : AppCompatActivity() {
+      lateinit var viewModell: ViewModell
+      lateinit var viewModelfact: viewModelfact
+
+      lateinit var activityMainBinding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        callApi()
+    }
+
+    private fun callApi() {
+       viewModell=ViewModelProvider(this).get(ViewModell::class.java)
+       viewModell.callapibyView(2)
+
+
+       viewModell.liveData.observe(this,{
+           it.apply {
+
+           }
+       })
     }
 }
