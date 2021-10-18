7c986f5235247564b93d2cd1b483a0af4e46a506
umangsh28 <im.mrstark08@gmail.com>
2021-10-18 15:50:13 +0530

    comit @3:50pm

--- app/src/main/java/com/example/codingoct18/MainActivity.kt
+++ app/src/main/java/com/example/codingoct18/MainActivity.kt
@@ -4,14 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.paging.PagingData
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.codingoct18.databinding.ActivityMainBinding
+import com.example.codingoct18.model.ResponseDTO
+import com.example.codingoct18.model.ResponseDTOItem
+import com.example.codingoct18.recycler.pagingadapter
 import com.example.codingoct18.viewmodel.ViewModell
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
       lateinit var viewModell: ViewModell
 
 
       lateinit var activityMainBinding: ActivityMainBinding
+
+      lateinit var pagingadapter: pagingadapter
+
+      var responseitem= mutableListOf<ResponseDTOItem>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,15 +34,33 @@ class MainActivity : AppCompatActivity() {
         callApi()
     }
 
+
+
     private fun callApi() {
        viewModell=ViewModelProvider(this).get(ViewModell::class.java)
        viewModell.callapibyView(2)
 
+        setRecycler()
+
+
 
        viewModell.liveData.observe(this,{
            it.apply {
-               activityMainBinding.text1.text=it.get(1).name.toString()
+               CoroutineScope(Dispatchers.IO).launch {
+                   pagingadapter.submitData(lifecycle,it)
+               }
            }
        })
+    }
+
+    private fun setRecycler() {
+
+        pagingadapter= pagingadapter()
+        val linearLayoutManager=LinearLayoutManager(this)
+
+        activityMainBinding.recycler.apply {
+            layoutManager=linearLayoutManager
+            this.adapter=pagingadapter
+        }
     }
 }
--- app/src/main/java/com/example/codingoct18/recycler/pagingadapter.kt
+++ app/src/main/java/com/example/codingoct18/recycler/pagingadapter.kt
@@ -1,22 +1,38 @@
 package com.example.codingoct18.recycler
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
+import com.example.codingoct18.R
+import com.example.codingoct18.databinding.ItemDesignBinding
 import com.example.codingoct18.model.ResponseDTO
+import com.example.codingoct18.model.ResponseDTOItem
 
-class pagingadapter  : PagingDataAdapter<ResponseDTO,holder>(){
+class pagingadapter()  : PagingDataAdapter<ResponseDTOItem,holder>(diff){
 
     companion object{
-        val diff=object : DiffUtil.ItemCallback<ResponseDTO>(){
-            override fun areItemsTheSame(oldItem: ResponseDTO, newItem: ResponseDTO): Boolean {
-               return oldItem
+        val diff=object : DiffUtil.ItemCallback<ResponseDTOItem>(){
+            override fun areItemsTheSame(oldItem: ResponseDTOItem, newItem: ResponseDTOItem): Boolean {
+               return oldItem.id==newItem.id
             }
 
-            override fun areContentsTheSame(oldItem: ResponseDTO, newItem: ResponseDTO): Boolean {
-                TODO("Not yet implemented")
+            override fun areContentsTheSame(oldItem: ResponseDTOItem, newItem: ResponseDTOItem): Boolean {
+                return oldItem.id==newItem.id && oldItem.name == newItem.name
             }
 
         }
+    }
+
+    override fun onBindViewHolder(holder: holder, position: Int) {
+        val res:ResponseDTOItem=getItem(position)!!
+        holder.setdata(res)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): holder {
+        val itemDesignBinding :ItemDesignBinding =DataBindingUtil.inflate(LayoutInflater.from(parent.context),R.layout.item_design,parent,false)
+        return holder(itemDesignBinding)
     }
 
 
