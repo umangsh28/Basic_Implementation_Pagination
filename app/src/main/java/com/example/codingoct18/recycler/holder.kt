987154c82a2811dc9853fb926b83db09b1b8fe4e
umangsh28 <im.mrstark08@gmail.com>
2021-10-18 15:28:17 +0530

    comit2 @3:30pm

--- app/src/main/java/com/example/codingoct18/recycler/holder.kt
+++ app/src/main/java/com/example/codingoct18/recycler/holder.kt
@@ -1,4 +1,17 @@
 package com.example.codingoct18.recycler
 
-class holder {
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.codingoct18.databinding.ItemDesignBinding
+import com.example.codingoct18.model.ResponseDTOItem
+
+class holder(private val itemDesignBinding: ItemDesignBinding):RecyclerView.ViewHolder(itemDesignBinding.root) {
+
+    fun setdata(responseDTOItem: ResponseDTOItem){
+        itemDesignBinding.dataa=responseDTOItem
+        Glide.with(itemDesignBinding.img1).load(responseDTOItem.image.original).into(itemDesignBinding.img1)
+
+    }
+
 }
--- app/src/main/java/com/example/codingoct18/recycler/pagingadapter.kt
+++ app/src/main/java/com/example/codingoct18/recycler/pagingadapter.kt
@@ -1,4 +1,23 @@
 package com.example.codingoct18.recycler
 
-class pagingadapter {
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import com.example.codingoct18.model.ResponseDTO
+
+class pagingadapter  : PagingDataAdapter<ResponseDTO,holder>(){
+
+    companion object{
+        val diff=object : DiffUtil.ItemCallback<ResponseDTO>(){
+            override fun areItemsTheSame(oldItem: ResponseDTO, newItem: ResponseDTO): Boolean {
+               return oldItem
+            }
+
+            override fun areContentsTheSame(oldItem: ResponseDTO, newItem: ResponseDTO): Boolean {
+                TODO("Not yet implemented")
+            }
+
+        }
+    }
+
+
 }
