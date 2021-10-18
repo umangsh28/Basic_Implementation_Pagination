b7a74d74b789030cb705f31368c2ff93a7b366d2
umangsh28 <im.mrstark08@gmail.com>
2021-10-18 14:22:21 +0530

    comit2 @2:22pm

--- app/src/main/java/com/example/codingoct18/api/ApiClient.kt
+++ app/src/main/java/com/example/codingoct18/api/ApiClient.kt
@@ -1,7 +1,13 @@
 package com.example.codingoct18.api
 
+import com.example.codingoct18.model.ResponseDTO
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface ApiClient {
+    //https://api.tvmaze.com/people?page=1
 
-
+    @GET()
+    fun getAll(): Call<ResponseDTO>
 
 }
